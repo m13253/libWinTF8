ca4892ddbb589177e31070ef97b5163817a0ce86
Star Brilliant <m13253@hotmail.com>
2014-11-23 23:37:08 +0800

    Fix for fucking MSVC

--- src/termio.cpp
+++ src/termio.cpp
@@ -231,8 +231,11 @@ private:
 
 class ConsoleInput : public std::istream {
 public:
-    ConsoleInput() = delete;
+    ConsoleOutput() :
+        std::istream() {
+    }
     ConsoleInput(std::ostream *tied_stream = nullptr) :
+        std::istream(),
         WTF8_buffer(new ConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE))) {
         rdbuf(WTF8_buffer.get());
         if(tied_stream)
@@ -244,8 +247,11 @@ private:
 
 class ConsoleOutput : public std::ostream {
 public:
-    ConsoleOutput() = delete;
+    ConsoleOutput() :
+        std::ostream() {
+    }
     ConsoleOutput(int fd = 1, std::ostream *tied_stream = nullptr) :
+        std::ostream(),
         WTF8_buffer(new ConsoleOutputBuffer(GetStdHandle(fd != 2 ? STD_OUTPUT_HANDLE : STD_ERROR_HANDLE))) {
         rdbuf(WTF8_buffer.get());
         if(tied_stream)
