13f0a15761149749c4c31653c9f02b25cc4bc067
Star Brilliant <m13253@hotmail.com>
2014-11-29 22:43:05 +0800

    Update

--- src/libwintf8.h
+++ src/libwintf8.h
@@ -24,6 +24,7 @@
 #include "libwintf8/concp.h"
 #include "libwintf8/env.h"
 #include "libwintf8/fileio.h"
+#include "libwintf8/printf.h"
 #include "libwintf8/spawn.h"
 #include "libwintf8/streamio.h"
 #include "libwintf8/termio.h"
